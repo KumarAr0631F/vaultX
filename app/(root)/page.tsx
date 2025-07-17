cdf07c9741383b33ae25d9a7cc517c6540d18cb6
KumarAr0631F <nitin.kumar132005@gmail.com>
2025-07-17 21:55:14 +0530

    implementing home UI with chart js

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,8 +1,26 @@
+import HeaderBox from '@/components/HeaderBox'
+import TotalBalanceBox from '@/components/TotalBalanceBox';
 import React from 'react'
 
 const Home = () => {
+  const loggedIn = {firstName: 'Nitin'};
+
   return (
-    <div className='text-red-500'>Home</div>
+    <section className="home">
+      <div className='home-content'>
+        <header className='home-header'>
+          <HeaderBox 
+            type="greeting"
+            title="Welcome"
+            user={loggedIn?.firstName || 'Guest'}
+            subtext="Access your manage your account and transactions efficiently."
+          />
+
+          <TotalBalanceBox accounts={[]} totalBanks={1} totalCurrentBalance={1500.35}/>
+        </header>
+         
+      </div>
+    </section>
   )
 }
 
